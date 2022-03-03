93a481783337c7c089943e30d62c9661e8a3fd30
1828Boxerbots <1828boxerbots@gmail.com>
2022-03-02 17:24:28 -0700

    STUFFANDTHINGS TM

--- src/main/cpp/subsystems/ShooterSubsystem.cpp
+++ src/main/cpp/subsystems/ShooterSubsystem.cpp
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project. 
 
 #include "subsystems/ShooterSubsystem.h"
-
+#include "Util.h"
 ShooterSubsystem::ShooterSubsystem(){}
 
 // This method will be called once per scheduler run
@@ -11,7 +11,7 @@ void ShooterSubsystem::Periodic() {}
 
 void ShooterSubsystem::Init()
 {
-#ifndef NOHW
+#ifndef NOHW_SENSORS
     m_shooterEncoder.SetDistancePerPulse(m_DISPULSE_SHOOT);
 #endif
     ResetEncoder();
@@ -20,14 +20,14 @@ void ShooterSubsystem::Init()
 // ENCODER FUNCTIONS
 void ShooterSubsystem::ResetEncoder()
 {
-#ifndef NOHW
+#ifndef NOHW_SENSORS
     m_shooterEncoder.Reset();
 #endif
 }
 
 double ShooterSubsystem::GetShooterSpeed()
 {
-#ifndef NOHW
+#ifndef NOHW_SENSORS
     //Units are distance PER SECOND as scaled by SetDistancePerPulse()
     frc::SmartDashboard::PutNumber("ShooterSpeed", m_shooterEncoder.GetRate());
     return m_shooterEncoder.GetRate();
@@ -36,14 +36,19 @@ double ShooterSubsystem::GetShooterSpeed()
 
 double ShooterSubsystem::GetShootDistance()
 {
-#ifndef NOHW
+#ifndef NOHW_SENSORS
     frc::SmartDashboard::PutNumber("ShooterDistance", m_shooterEncoder.GetDistance());
     return m_shooterEncoder.GetDistance();
 #endif
 }
-
+void ShooterSubsystem::ShootToTimer(double time, double speed){
+m_shooterMotor.Set(speed);
+    Util::DelayInSeconds((units::time::second_t) time);
+m_shooterMotor.Set(0.00000);
+}
 void ShooterSubsystem::SetShooterSpeed(double targetSpeed)
 {
+    #ifndef NOHW_SENSORS
     //Using a linear function speed= (78)power + 0 (found testing different powers and graphing their speeds)
     // y=mx+b -> x= (y+b)/m
     double power = targetSpeed / 78;
@@ -53,11 +58,12 @@ void ShooterSubsystem::SetShooterSpeed(double targetSpeed)
     {
         GetShooterSpeed();
     }
+    #endif
 }
 
 int ShooterSubsystem::GetEncoderRaw()
 {
-#ifndef NOHW
+#ifndef NOHW_SENSORS
     frc::SmartDashboard::PutNumber("ShooterRaw", m_shooterEncoder.GetRaw());
     return m_shooterEncoder.GetRaw();
 #endif
@@ -66,7 +72,7 @@ int ShooterSubsystem::GetEncoderRaw()
 // SHOOTER FUNCTIONS
 void ShooterSubsystem::ShootMotor(double speed)
 {
-#ifndef NOHW
+#ifndef NOHW_SENSORS
     m_shooterMotor.Set(speed);
 #endif
 }
